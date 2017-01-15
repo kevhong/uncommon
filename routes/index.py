7b8609f58205f0ef2d54eec2c46f1ffb3b5734f1
Nancy Cheng <nancycheng@wireless-s0-no-150-3-41.uchicago.edu>
2017-01-15 02:46:27 -0600

    slight modifications to dogify_url

--- routes/index.py
+++ routes/index.py
@@ -6,7 +6,7 @@ from . import routes
 
 from src import utils
 from src import face
-
+from json import loads
 count = 0
 
 index_val = 0
@@ -39,16 +39,28 @@ def dogify_func():
 
 @routes.route('/dogifyurl', methods = ['POST'])
 def dogify_url():
-    link = request.form['link']
-    filename = os.path.join("data", "saved", str(hash(link)) + ".jpg")
-    urllib.urlretrieve(link, filename)
-    key = str(hash(link))
-    filename = face.overlay_face(filename, imageOverlay = os.path.join("data",
-         "overlays", "dog_face_tongue.png"), test = False, ret = key)
-    print filename
-    return "http://127.0.0.1:5000/files/" + key
+    data = loads(request.data)
+    link = data['link']
+    key = ""
+    ret_val = 200
+    try:
+        #print link
+        filename = os.path.join("data", "saved", str(hash(link)) + ".jpg")
+        urllib.urlretrieve(link, filename)
+        filename = face.overlay_face(filename, imageOverlay = os.path.join("data",
+             "overlays", "dog_face_tongue.png"), test = False, ret = key)
+        key = str(hash(link))
+    except:
+        key = link
+        ret_val = 404
+        #print filename
+    data = {'link': "http://127.0.0.1:5000/files/" + key}
+    resp = jsonify(data)
+    resp.status_code = ret_val
+    print ret_val
+    return resp
 
 @routes.route("/files/<string:index>", methods = ['GET'])
 def dogify_return(index):
-    filename = os.path.join("data", "created",index + ".jpg")
-    return send_file(filename);
+    filename = os.path.join("data", "created", index + ".jpg")
+    return send_file(filename)
